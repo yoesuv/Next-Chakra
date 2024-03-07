c47c55f930188d3ba7b19ed05d8948da5908efb1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-03-07 16:10:31 +0700

    :newspaper: list post service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import UseListPost from "@/networks/list-post-service";
 import { Heading } from "@chakra-ui/react";
 
 export default function Home() {
+  const { data, isLoading, isError } = UseListPost();
+
   return (
     <main>
       <div>
-        <Heading>Next JS & Chakra UI</Heading>
+        <Heading>Next JS & Chakra UI {data?.length}</Heading>
       </div>
     </main>
   );
