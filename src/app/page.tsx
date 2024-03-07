443445ac89c15d4fe7f96d047a9ca484357e4b25
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-03-07 16:53:16 +0700

    :sparkles: show list post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import UseListPost from "@/networks/list-post-service";
-import { Heading } from "@chakra-ui/react";
+import { Table, Tbody, Tr, Th, TableContainer, Button } from "@chakra-ui/react";
 
 export default function Home() {
   const { data, isLoading, isError } = UseListPost();
 
   return (
-    <main>
-      <div>
-        <Heading>Next JS & Chakra UI {data?.length}</Heading>
-      </div>
-    </main>
+    <TableContainer>
+      <Table>
+        <Tbody>
+          {data?.map((item, index) => (
+            <Tr>
+              <Th>{index + 1}</Th>
+              <Th>{item.title}</Th>
+              <Th>
+                <Button size="xs">Detail</Button>
+              </Th>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
   );
 }
