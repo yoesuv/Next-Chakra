3f9025723a1180a3aa0b88a72f8ea7ab43873a17
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-03-09 14:04:57 +0700

    :construction: form input field

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,5 +1,38 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+} from "@chakra-ui/react";
 
 export default function Create() {
-  return <Heading>Create</Heading>;
+  return (
+    <Container centerContent={true}>
+      <Heading mt={10}>Create Post</Heading>
+      <FormControl isRequired mt={4}>
+        <FormLabel>Post Title</FormLabel>
+        <Input type="text" name="title" placeholder="input title here" />
+      </FormControl>
+      <FormControl isRequired mt={4}>
+        <FormLabel>Post Body</FormLabel>
+        <Input type="text" name="body" placeholder="input body here" />
+      </FormControl>
+      <FormControl isRequired mt={4}>
+        <FormLabel>User Id</FormLabel>
+        <NumberInput>
+          <NumberInputField name="userId" placeholder="input user id here" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
+    </Container>
+  );
 }
