8b3a9202db8c50488c6e735bb210252b58c8002d
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-03-10 10:08:10 +0700

    :pencil: api CRUD post

--- src/networks/post-service.tsx
+++ src/networks/post-service.tsx
@@ -12,6 +12,21 @@ async function fetchDetailPosts(id: number): Promise<PostModel> {
   return result.data;
 }
 
+export const createPost = async (post: PostModel): Promise<PostModel> => {
+  const result = await client.post("posts", JSON.stringify(post));
+  return result.data;
+};
+
+export const updatePost = async (post: PostModel): Promise<PostModel> => {
+  const result = await client.put("posts/" + post.id, JSON.stringify(post));
+  return result.data;
+};
+
+export const deletePost = async (id: number): Promise<PostModel> => {
+  const result = await client.delete("posts/" + id);
+  return result.data;
+};
+
 export const UseListPost = () =>
   useQuery({
     queryKey: ["listPost"],
