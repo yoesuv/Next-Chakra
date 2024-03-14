cb6f9623c60c79c42e36433a4f2c0795509bf368
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-03-14 09:27:07 +0700

    :sparkles: load photo gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,5 +1,40 @@
-import { Heading } from "@chakra-ui/react";
+"use client";
+
+import { UseListPhoto } from "@/networks/photo-service";
+import {
+  Box,
+  Card,
+  CardBody,
+  Container,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 
 export default function Gallery() {
-  return <Heading>Gallery</Heading>;
+  const { data, isLoading } = UseListPhoto();
+
+  if (isLoading) {
+    return <Heading>Loading...</Heading>;
+  }
+
+  return (
+    <Container maxW="container.xl">
+      <SimpleGrid minChildWidth={250} gap={4}>
+        {data?.map((item, index) => (
+          <Box key={index}>
+            <Card shadow="sm">
+              <CardBody>
+                <Image src={item.url} />
+                <Text mt={4} fontWeight="medium" fontSize="md" noOfLines={1}>
+                  {item.title}
+                </Text>
+              </CardBody>
+            </Card>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Container>
+  );
 }
